64e42ce8d0c1fd2bb848a668f318eafe7552fe1a
Khriplivez Artem <lep12345@mail.ru>
2021-11-22 17:14:41 +0300

    Add task text to task classes

--- olymp/Program.cs
+++ olymp/Program.cs
@@ -8,37 +8,7 @@ namespace olymp
     {
         static void Main(string[] args)
         {
-            Run();
-        }
-
-        public static void Run()
-        {
-            var input = Console.ReadLine().Split(" ");
-            int n = int.Parse(input[0]);
-            int m = int.Parse(input[1]);
-            List<int> rawData = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToList();
-            List<int> count1 = new List<int>();
-            List<int> count0 = new List<int>();
-
-
-            for (int i = 0; i < m; i++)
-            {
-                count0.Add(0);
-                count1.Add(0);
-            }
-            for (int i = 0; i < rawData.Count; i++)
-            {
-                if (rawData[i] == 1)
-                    count1[i % m]++;
-                else
-                    count0[i % m]++;
-            }
-            int ans = 0;
-            for (int i = 0; i < m; i++)
-            {
-                ans += Math.Min(count1[i], count0[i]);
-            }
-            Console.WriteLine(ans);
+            Task1.Run();
         }
     }
 }
